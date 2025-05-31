139dd081f0548b0458bea553945874b709a5b59d
Neha Jagtap <nehajagtap549@gmail.com>
2025-05-31 17:44:02 -0400

    Added Langsmith for debugging and monitoring

--- src/rag_pipeline/test.py
+++ src/rag_pipeline/test.py
@@ -1,12 +1,12 @@
 from graph.builder import build_app
 
 def test_query(question: str):
-    print(f"\n🔍 Running adaptive RAG pipeline for question: {question}")
+    print(f"\n Running adaptive RAG pipeline for question: {question}")
     app = build_app()
 
     result = app.invoke({"question": question})
 
-    print("\n✅ Final Answer:")
+    print("\n Final Answer:")
     print(result["generation"])
     return result
 
